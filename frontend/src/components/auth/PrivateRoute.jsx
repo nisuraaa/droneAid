5cc078c66a8b1d807f433dc7ac1e044eaa44e91c
Nisura Indisa <nisuraindisa21@gmail.com>
2023-12-08 22:36:19 +0530

    Update PrivateRoute.jsx

--- frontend/src/components/auth/PrivateRoute.jsx
+++ frontend/src/components/auth/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useAuthContext } from "@asgardeo/auth-react";
 import React, { useEffect, useState } from 'react';
-
+import { Flex } from '@chakra-ui/react';
 export const PrivateRoute = ({ children, allowedRoles }) => {
     const { state, getBasicUserInfo, signOut } = useAuthContext();
     const [userRole, setUserRole] = useState(null);
